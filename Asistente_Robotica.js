f90e955f3fb07211890809c317ba080cbc47de7a
dsilva25 <dsilvacontreras25@gmail.com>
2018-08-18 17:27:01 -0300

    Modficaciones

--- Asistente_Robotica.js
+++ Asistente_Robotica.js
@@ -1,22 +1,39 @@
 'use strict';
 
+function respTextToSpeech(text) {
+    const textToSpeech = require('@google-cloud/text-to-speech');
+    const fs = require('fs');
+
+    const client = new textToSpeech.TextToSpeechClient();
+
+    const outputFile = './output.mp3';
+
+    const request = {
+    input: {text: text},
+    voice: {languageCode: 'es-CL', ssmlGender: 'FEMALE'},
+    audioConfig: {audioEncoding: 'MP3'},
+    };
+
+    client.synthesizeSpeech(request, (err, response) => {
+        if (err) {
+            console.error('ERROR:', err);
+            return;
+        }
+
+        fs.writeFile(outputFile, response.audioContent, 'binary', err => {
+            if (err) {
+            console.error('ERROR:', err);
+            return;
+            }
+            console.log(`Audio content written to file: ${outputFile}`);
+        });
+    });
+}
+
 function streamingRecognize(filename, encoding, sampleRateHertz, languageCode) {
-  // [START speech_streaming_recognize]
   const fs = require('fs');
-
-  // Imports the Google Cloud client library
   const speech = require('@google-cloud/speech');
-
-  // Creates a client
   const client = new speech.SpeechClient();
-
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  // const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
-  // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
-  // const sampleRateHertz = 16000;
-  // const languageCode = 'BCP-47 language code, e.g. en-US';
 
   const request = {
     config: {
@@ -24,10 +41,9 @@ function streamingRecognize(filename, encoding, sampleRateHertz, languageCode) {
       sampleRateHertz: sampleRateHertz,
       languageCode: languageCode,
     },
-    interimResults: false, // If you want interim results, set this to true
+    interimResults: false,
   };
 
-  // Stream the audio to the Google Cloud Speech API
   const recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
@@ -72,12 +88,18 @@ function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
   const recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
-    .on('data', data =>
+    .on('data', data => {
+      const transcripcion = data.results[0].alternatives[0].transcript;
+      if (transcripcion.includes('hora')) {
+        respTextToSpeech('La hora es: 17:04');
+      }
+
       process.stdout.write(
         data.results[0] && data.results[0].alternatives[0]
           ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
           : `\n\nReached transcription time limit, press Ctrl+C\n`
       )
+    }
     );
 
   // Start recording and send the microphone input to the Speech API
@@ -85,16 +107,14 @@ function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
     .start({
       sampleRateHertz: sampleRateHertz,
       threshold: 0,
-      // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
       verbose: false,
-      recordProgram: 'rec', // Try also "arecord" or "sox"
+      recordProgram: 'rec',
       silence: '10.0',
     })
     .on('error', console.error)
     .pipe(recognizeStream);
 
   console.log('Listening, press Ctrl+C to stop.');
-  // [END speech_streaming_mic_recognize]
 }
 
 require(`yargs`)
---
+++ Prueba.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import time
+
+x = raw_input("Presione la tecla b :")
+while True:
+	if x == "b":
+		print "Iniciar Programa"
+		time.sleep(1.0)
+
